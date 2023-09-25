6fc43b431a88ba84240eb4dbcd645052c3c69e0d
laith alfar <laith.alfar12@gmail.com>
2023-09-25 20:50:29 +0100

    Set Github

---
+++ src/main/java/com/flappybirds/flappybirds/FlappyBirds.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.flappybirds.flappybirds;
+import javax.swing.JFrame;
+/**
+ *
+ * @author laith
+ */
+
+
+public class FlappyBirds {
+    
+    public static FlappyBirds flappyBirds; 
+
+    public final int width = 800, height = 1200;
+    
+    public FlappyBirds(){
+    
+    JFrame jframe = new JFrame();
+    
+    jframe.setSize(width,height);
+    jframe.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        
+        flappyBirds = new FlappyBirds();
+        System.out.println("Hello World!");
+    }
+}
