c709c36571a0cf0815fdf5fb2a44c07e4d558d2e
laithalfar <laith.alfar12@gmail.com>
2023-10-01 13:49:41 +0100

    create Render Classand apply JPanel

---
+++ src/main/java/com/flappybirds/flappybirds/Renderer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.flappybirds.flappybirds;
+
+import javax.swing.JPanel;
+
+
+/**
+ *
+ * @author laith
+ * 
+ * This is a class for rendering 
+ */
+
+//extend the class with JPanel which is a container that can
+//store several components
+public class Renderer extends JPanel{
+    
+    private static final long serialVersionUID = 1L; //assigning the class a serial number, which is used during deserialization to verify that the sender and receiver of a serialized object have loaded classes for that object that are compatible with respect to serialization.
+    
+}
